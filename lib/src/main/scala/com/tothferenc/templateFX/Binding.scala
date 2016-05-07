6afac10396b78630206048007207cfbd0610604c
Ferenc Tóth <feri@gawker.com>
2016-05-07 13:32:24 +0200

    ability to check satisfaction of constraints

--- lib/src/main/scala/com/tothferenc/templateFX/Binding.scala
+++ lib/src/main/scala/com/tothferenc/templateFX/Binding.scala
@@ -4,14 +4,19 @@ import javafx.scene.Node
 
 import com.tothferenc.templateFX.attribute.{ Attribute, Unsettable }
 
-abstract class Constraint[-FXType] extends (FXType => Option[Change]) {
+abstract class Constraint[-T] extends (T => Option[Change]) {
   def attribute: Unsettable[_]
+  def isSatisfiedBy(item: T): Boolean
 }
 
-final case class Binding[FXType <: Node, Attr](attribute: Attribute[FXType, Attr], value: Attr) extends Constraint[FXType] {
-  override def apply(fxObj: FXType): Option[Change] =
-    if (Option(attribute.read(fxObj)) != Option(value))
-      Some(Mutate(fxObj, attribute, value))
+final case class Binding[Item <: Node, Attr](attribute: Attribute[Item, Attr], value: Attr) extends Constraint[Item] {
+
+  override def isSatisfiedBy(item: Item): Boolean =
+    Option(attribute.read(item)) != Option(value)
+
+  override def apply(item: Item): Option[Change] =
+    if (isSatisfiedBy(item))
+      Some(Mutate(item, attribute, value))
     else
       None
 }
