8a39680efbf76a1e414e6b782c081b4528de0f18
Ferenc Tóth <feri@gawker.com>
2016-05-18 18:24:15 +0200

    lib: make Template parametric

--- lib/src/main/scala/com/tothferenc/templateFX/specs/Spec.scala
+++ lib/src/main/scala/com/tothferenc/templateFX/specs/Spec.scala
@@ -6,23 +6,18 @@ import com.tothferenc.templateFX._
 import com.tothferenc.templateFX.userdata.ManagedAttributes
 import com.tothferenc.templateFX.userdata.UserDataAccess
 
-abstract class Template {
-  type Output
-  def materialize(): Output
+abstract class Template[T] {
+  def materialize(): T
   def reconcilationSteps(other: Node): Option[List[Change]]
 }
 
-object Template {
-  type Aux[Out] = Template { type Output = Out }
-}
+abstract class ConstraintBasedReconcilation[T] extends Template[T] {
 
-abstract class ConstraintBasedReconcilation extends Template {
+  implicit def userDataAccess: UserDataAccess[T]
 
-  implicit def userDataAccess: UserDataAccess[Output]
+  def constraints: Seq[Constraint[T]]
 
-  def constraints: Seq[Constraint[Output]]
-
-  def requiredChangesIn(item: Output): List[Change] = {
+  def requiredChangesIn(item: T): List[Change] = {
     val featuresToRemove = ManagedAttributes.get(item) match {
       case Some(features) =>
         features.filterNot { checked =>
@@ -35,24 +30,23 @@ abstract class ConstraintBasedReconcilation extends Template {
     val featureUpdates = constraints.flatMap(_.apply(item))
 
     if (featureUpdates.nonEmpty || featuresToRemove.nonEmpty)
-      List(Mutation[Output](item, featureUpdates, featuresToRemove))
+      List(Mutation[T](item, featureUpdates, featuresToRemove))
     else
       Nil
   }
 }
 
-abstract class Spec[FXType <: Node] extends ConstraintBasedReconcilation {
-
-  type Output = FXType
+abstract class Spec[FXType <: Node] extends ConstraintBasedReconcilation[FXType] {
 
   implicit def specifiedClass: Class[FXType]
   def materialize(): FXType
 
   override def reconcilationSteps(otherItem: Node): Option[List[Change]] = {
     otherItem match {
-      case expected: Output @unchecked if specifiedClass == expected.getClass =>
+      case expected: FXType @unchecked if specifiedClass == expected.getClass =>
         Some(requiredChangesIn(expected))
-      case _ => None
+      case _ =>
+        None
     }
   }
 }
